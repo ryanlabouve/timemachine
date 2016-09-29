7521e619e9ec81e1bc790412893656dfbf09cbfe
Ryan LaBouve <imryanlabouve@gmail.com>
2016-09-29 16:22:18 -0500

    more brutalization

--- timemachine.js
+++ timemachine.js
@@ -1,18 +1,12 @@
 (function(name, definition) {
-  if (typeof define === 'function') { // AMD
-    define(definition);
-  } else if (typeof module !== 'undefined' && module.exports) { // Node.js
-    module.exports = definition();
-  } else { // Browser
-    var timemachine = definition(),
-      global = this,
-      old = global[name];
-    timemachine.noConflict = function() {
-      global[name] = old;
-      return timemachine;
-    };
-    global[name] = timemachine;
-  }
+  var timemachine = definition(),
+    global = this,
+    old = global[name];
+  timemachine.noConflict = function() {
+    global[name] = old;
+    return timemachine;
+  };
+  global[name] = timemachine;
 })('timemachine', function() {
 
   var OriginalDate = Date,
